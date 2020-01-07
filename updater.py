0e1067200394bf6591611f9d04b1da9b96e4c993
Ed Luff <beartechtalks@gmail.com>
2020-01-07 19:01:18 +0000

    Create updater.py

---
+++ updater.py
@@ -0,0 +1,26 @@
+import subprocess
+
+# get some variables
+ishCVer = ""
+ishCBuild = ""
+ishNVer = ""
+ishNBuild = ""
+
+if ishCVer == "":
+    ishCVer = input("Old iSH version:")
+if ishCBuild == "":
+    ishCBuild = input("Old iSH build:")
+if ishNVer == "":
+    ishNVer = input("New iSH version:")
+if ishNBuild == "":
+    ishNBuild = input("New iSH build:")
+
+# update the motd
+subprocess.check_call("sed 's/{}/{}/g' motd".format(ishCVer, iSHNver), shell=True)
+subprocess.check_call("sed 's/{}/{}/g' motd".format(ishCBuild, iSHNBuild), shell=True)
+
+# update alpine (currently only supports the index/the edge repository)
+subprocess.check_call("apk update")
+supprocess.check_call("apk upgrade --available && sync")
+
+print("Update complete, restart iSH to see changes.")
