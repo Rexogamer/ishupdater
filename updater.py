1673993fce75b9dc84c41f2f7b0dea30a015d731
Ed Luff <beartechtalks@gmail.com>
2020-03-09 17:30:48 +0000

    feat: add args and updating alpine (#4)
    * feat: add args (WIP) + properly support updating alpine
    
    * fix: syntax
    
    * Update updater.py
    
    * fix(?): no response
    
    * fix: indentation
    
    * fix: should work
    
    * fix: forgot import-ant line
    
    * don’t conflict with argparse
    
    * indentation is the bane of my existence 
    
    not sure if JS’s semicolons are much better though
    
    * Update updater.py

--- updater.py
+++ updater.py
@@ -1,26 +1,61 @@
+import argparse
 import subprocess
+import sys
 
-# get some variables
-ishCVer = ""
-ishCBuild = ""
-ishNVer = ""
-ishNBuild = ""
+class Updater:  
+    # arguments
+    def arguments(self):
+        p = argparse.ArgumentParser(add_help=True)
+        p.add_argument('-all',action='store_true',help='Updates everything.')
+        p.add_argument('-noapk',action='store_true',help="Doesn't update Alpine.")
+        p.add_argument('-nomotd',action='store_true',help="Doesn't update the MOTD.")
+        args = p.parse_args()
+        if args.all:
+            self.updateall()
+        elif args.noapk:
+            self.updatemotd()
+        elif args.nomotd:
+            self.updatealpine()
+        if len(sys.argv[0]) != 1:
+            print('Run python3 updater.py --help to see the options.')
+    # the motd updater
+    def updatemotd(self):
+        ishCVer = ""
+        ishCBuild = ""
+        ishNVer = ""
+        ishNBuild = ""
+        if ishCVer == "":
+            ishCVer = input("Old iSH version: ")
+        if ishCBuild == "":
+            ishCBuild = input("Old iSH build: ")
+        if ishNVer == "":
+            ishNVer = input("New iSH version: ")
+        if ishNBuild == "":
+            ishNBuild = input("New iSH build: ")
 
-if ishCVer == "":
-    ishCVer = input("Old iSH version: ")
-if ishCBuild == "":
-    ishCBuild = input("Old iSH build: ")
-if ishNVer == "":
-    ishNVer = input("New iSH version: ")
-if ishNBuild == "":
-    ishNBuild = input("New iSH build: ")
+        # update the motd
+        subprocess.check_call("sed 's/{}/{}/g' /etc/motd".format(ishCVer, ishNVer), shell=True)
+        subprocess.check_call("sed 's/{}/{}/g' /etc/motd".format(ishCBuild, ishNBuild), shell=True)
+        print("MOTD updated; restart to see changes.")
 
-# update the motd
-subprocess.check_call("sed 's/{}/{}/g' /etc/motd".format(ishCVer, ishNVer), shell=True)
-subprocess.check_call("sed 's/{}/{}/g' /etc/motd".format(ishCBuild, ishNBuild), shell=True)
+    # the alpine updater
+    def updatealpine(self):
+        apkCVer = ""
+        apkNVer = ""
+        if apkCVer == "":
+            apkCVer = input("Current Alpine version: ")
+        if apkNVer == "":
+            apkNVer = input("New Alpine version: ")
 
-# update alpine (currently only supports the index/the edge repository)
-subprocess.check_call("apk update", shell=True)
-subprocess.check_call("apk upgrade --available && sync", shell=True)
+        # update alpine
+        subprocess.check_call("sed 's/{}/{}/g' /etc/apk/repositories".format(apkCVer, apkNVer), shell=True)
+        subprocess.check_call("apk update", shell=True)
+        subprocess.check_call("apk upgrade --available && sync", shell=True)
+        print("Updated Alpine.")
 
-print("Update complete, restart iSH to see changes.")
+    def updateall(self):
+        self.updatemotd()
+        self.updatealpine()
+            
+if __name__ == "__main__":
+  Updater().arguments()
